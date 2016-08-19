ebf7a714fd52ab5d1eeb6194e96bc2cf4aabb69a
Louis <louis.chenais@5emegauche.com>
2016-08-19 15:16:40 +0200

    Gulpfile Cleaned

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
 // ---------------------------------------------------------------
 // Include Plugins
 // ---------------------------------------------------------------
-var gulp = require('gulp');
-var sass             = require('gulp-sass');
-var sassdoc          = require('sassdoc');
-var plumber          = require('gulp-plumber');
-var gutil            = require('gulp-util');
-var concat           = require('gulp-concat');
-var sourcemaps       = require('gulp-sourcemaps');
-var autoprefixer     = require('gulp-autoprefixer');
-var cleanCSS         = require('gulp-clean-css');
-var stripCssComments = require('gulp-strip-css-comments');
-var uncss            = require('gulp-uncss');
-var browserSync      = require('browser-sync');
-var uglify           = require('gulp-uglify');
-var rename           = require('gulp-rename');
-var critical         = require('critical').stream;
-var imagemin         = require('gulp-imagemin');
-var svgstore         = require('gulp-svgstore');
-var svgmin           = require('gulp-svgmin');
-var rename           = require('gulp-rename');
-var size             = require('gulp-size');
+var gulp             = require( 'gulp' );
+var sass             = require( 'gulp-sass' );
+var sassdoc          = require( 'sassdoc' );
+var plumber          = require( 'gulp-plumber' );
+var gutil            = require( 'gulp-util' );
+var concat           = require( 'gulp-concat' );
+var sourcemaps       = require( 'gulp-sourcemaps' );
+var autoprefixer     = require( 'gulp-autoprefixer' );
+var cleanCSS         = require( 'gulp-clean-css' );
+var stripCssComments = require( 'gulp-strip-css-comments' );
+var uncss            = require( 'gulp-uncss' );
+var browserSync      = require( 'browser-sync' );
+var uglify           = require( 'gulp-uglify' );
+var rename           = require( 'gulp-rename' );
+var critical         = require( 'critical' ).stream;
+var imagemin         = require( 'gulp-imagemin' );
+var svgstore         = require( 'gulp-svgstore' );
+var svgmin           = require( 'gulp-svgmin' );
+var rename           = require( 'gulp-rename' );
+var size             = require( 'gulp-size' );
 
 // ---------------------------------------------------------------
 // Configuration
@@ -30,10 +30,15 @@ var path = {
   css: './app/assets/css/',
   js: './app/js/**/*.js',
   img: './app/assets/img/*',
+  icons: './app/assets/icons/*.svg',
+  fonts: './app/assets/fonts/*.{ttf,woff,eof,svg,otf}',
   html: './app/*.html',
-  dist: './dist/assets/',
+  php: './app/*.php',
+  dist: './dist/',
   dist_js: './dist/assets/js/',
-  dist_css: './dist/assets/css/'
+  dist_css: './dist/assets/css/',
+  dist_fonts: './dist/assets/fonts/',
+  dist_icons: './dist/assets/icons/',
 };
 
 var autoprefixerOptions = {
@@ -47,14 +52,14 @@ var reload = browserSync.reload;
 // see source : https://www.timroes.de/2015/01/06/proper-error-handling-in-gulp-js/
 var gulp_src = gulp.src;
 gulp.src = function() {
-  return gulp_src.apply(gulp, arguments)
-    .pipe(plumber(function(error) {
+  return gulp_src.apply( gulp, arguments )
+    .pipe( plumber(function(error) {
       // Output an error message
-      gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
+      gutil.log( gutil.colors.red('Error (' + error.plugin + '): ' + error.message) );
       // emit the end event, to properly end the task
-      this.emit('end');
-    })
-  );
+      this.emit( 'end');
+    } )
+  ) ;
 };
 
 // ---------------------------------------------------------------
@@ -83,55 +88,55 @@ gulp.task( 'watch', ['serve'], function(){
 // + refresh stream
 gulp.task('sass', function () {
   return gulp
-    .src(path.sass)
-    .pipe(sassdoc())
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(path.css))
-    .pipe(size())
-    .pipe(reload({stream: true}));
+    .src( path.sass )
+    .pipe( sassdoc() )
+    .pipe( sourcemaps.init() )
+    .pipe( sass() )
+    .pipe( autoprefixer(autoprefixerOptions) )
+    .pipe( sourcemaps.write() )
+    .pipe( gulp.dest(path.css) )
+    .pipe( size() )
+    .pipe( reload({stream: true}) );
 });
 
 // Production Sass Task : Compile SASS into CSS + Remove comments 
 // + Remove unused css + Autoprefixer
 // + Rename + Minify + Move to dest folder
-gulp.task('sass-prod', function () {
+gulp.task( 'sass-prod', function () {
   return gulp
-    .src(path.sass)
-    .pipe(sass({
+    .src( path.sass )
+    .pipe( sass({
       onError: console.error.bind(console, 'SASS error')
-    }))
-    .pipe(stripCssComments())
-    .pipe(uncss({
+    }) )
+    .pipe( stripCssComments() )
+    .pipe( uncss({
         html: ['app/index.html']
-    }))
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(rename({
+    }) )
+    .pipe( autoprefixer(autoprefixerOptions) )
+    .pipe( rename({
       suffix: '.min'
-    }))
-    .pipe(cleanCSS({debug: true}, function(details) {
-        console.log(details.name + ' original size : ' + details.stats.originalSize);
-        console.log(details.name + ' minified size : ' + details.stats.minifiedSize);
-    }))
-    .pipe(size())
-    .pipe(gulp.dest(path.dist + '/css'));
-});
+    }) )
+    .pipe( cleanCSS({debug: true}, function(details) {
+        console.log( details.name + ' original size : ' + details.stats.originalSize );
+        console.log( details.name + ' minified size : ' + details.stats.minifiedSize );
+    }) )
+    .pipe( size() )
+    .pipe( gulp.dest(path.dist + '/css') );
+} );
 
 // JS Prod Task = Minimify JS + Rename it + Move it to build/js
 // TODO : Copy vendor minified files in build/js 
 // + Concat files + Rename final file
-gulp.task('js-prod', function() {
+gulp.task( 'js-prod', function() {
   return gulp
-  .src(path.js)
-  .pipe(uglify())
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(gulp.dest(path.dist_js));
-});
+  .src( path.js )
+  .pipe( uglify() )
+  .pipe( rename({ suffix: '.min' }) )
+  .pipe( gulp.dest(path.dist_js) );
+} );
 
 // Compress Images
-gulp.task('img', function () {
+gulp.task( 'img', function () {
   return gulp
   .src(path.img)
   .pipe(imagemin({
@@ -139,21 +144,21 @@ gulp.task('img', function () {
     svgoPlugins: [{removeViewBox: false}],
   }))
   .pipe(gulp.dest(path.dist + '/img'));
-});
+} );
 
 // Sprite all the SVG inside the 'icons' folder
 // into a single SVG file in 'icons/dest'
-gulp.task('svgstore', function () {
+gulp.task( 'svgstore', function () {
   return gulp
     .src('assets/icons/*.svg')
     .pipe(svgmin())
     .pipe(svgstore())
     .pipe(rename({baseline: 'sprite'}))
     .pipe(gulp.dest('assets/icons/dest'));
-});
+} );
 
 // Generate & Inline Critical-path CSS
-gulp.task('critical', function () {
+gulp.task( 'critical', function () {
   return gulp
     .src(path.html)
     .pipe(critical({
@@ -166,10 +171,10 @@ gulp.task('critical', function () {
       }]
     }))
     .pipe(gulp.dest(path.dist));
-});
+} );
 
 // Run a Google Page Speed Insight Test for mobile
-gulp.task('mobile', function () {
+gulp.task( 'mobile', function () {
   return psi(site, {
     // key: key
     nokey: 'true',
@@ -178,10 +183,10 @@ gulp.task('mobile', function () {
     console.log(data.score);
     console.log(data.pageStats);
   });
-});
+} );
 
 // Run a Google Page Speed Insight Test for desktop
-gulp.task('desktop', function () {
+gulp.task( 'desktop', function () {
   return psi(site, {
     nokey: 'true',
     // key: key,
@@ -190,9 +195,30 @@ gulp.task('desktop', function () {
     console.log(data.score);
     console.log(data.pageStats);
   });
-});
+} );
 
-gulp.task('default', ['watch'], function () {});
+// Deleting all dist content
+gulp.task( 'clean', function() {
+  return del.sync( 'dist' );
+} );
 
-gulp.task('prod', ['sass-prod', 'js-prod', 'critical', 'img']);
+gulp.task( 'default', ['watch'], function () {} );
+
+gulp.task( 'build', ['clean', 'sass-prod', 'js-prod', 'critical', 'img'], function() {
+  // Copy PHP files to dist
+  gulp.src( path.php )
+    .pipe( gulp.dest( path.dist ) );
+
+  // Copy fonts files to dist
+  gulp.src( path.fonts )
+    .pipe(gulp.dest( path.dist_fonts ));
+
+  // Copy SVG icons to dist
+  gulp.src( path.icons )
+    .pipe( gulp.dest( path.dist_icons ) );
+    
+  // Copy SVG sprite & PNG fallbacks to dist
+  gulp.src( 'app/icons/dest/*.{svg,png}' )
+    .pipe( gulp.dest( path.dist_icons + 'dest/' ) );
+} );
 
